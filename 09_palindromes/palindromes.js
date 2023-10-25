06019f00d54fbb399bcb41b4c1deca5bd5b366f2
Mark Rodgers <markrodgers.dev@gmail.com>
2023-10-25 17:22:36 +0100

    Complete exercises 08-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const replacedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return replacedString === replacedString.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let total = 1;
+    let first = 1;
+    let second = 1;
 
+    if(number < 0){
+        return "OOPS"
+    }
+
+    
+
+    for(let i = 1; i <= number; i++){
+        if(i === 1 || i === 2){
+            continue;
+        }
+        total = first + second;
+        first = second;
+        second = total;
+
+
+    }
+    return total;
 };
 
 // Do not edit below this line
